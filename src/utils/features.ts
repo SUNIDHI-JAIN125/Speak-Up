85ffe0e6deed9004d199a86a7b2a54d2250618a8
Sunidhi Jain <130484301+SUNIDHI-JAIN125@users.noreply.github.com>
2023-11-26 11:07:31 +0530

    Update features.ts

--- src/utils/features.ts
+++ src/utils/features.ts
@@ -88,38 +88,38 @@ export const countMatchingElements = (
   return matchedCount;
 };
 
-export const fetchAudio = async (
-  text: string,
-  language: LangType
-): Promise<string> => {
-  const key = import.meta.env.VITE_TEXT_TO_SPEECH_API;
-  const rapidKey = import.meta.env.VITE_RAPID_API;
+// export const fetchAudio = async (
+//   text: string,
+//   language: LangType
+// ): Promise<string> => {
+//   const key = import.meta.env.VITE_TEXT_TO_SPEECH_API;
+//   const rapidKey = import.meta.env.VITE_RAPID_API;
 
-  const encodedParams = new URLSearchParams({
-    src: text,
-    r: "0",
-    c: "mp3",
-    f: "8khz_8bit_mono",
-    b64: "true",
-  });
+//   const encodedParams = new URLSearchParams({
+//     src: text,
+//     r: "0",
+//     c: "mp3",
+//     f: "8khz_8bit_mono",
+//     b64: "true",
+//   });
 
-  if (language === "ja") encodedParams.set("hl", "ja-jp");
-  else if (language === "es") encodedParams.set("hl", "es-es");
-  else if (language === "fr") encodedParams.set("hl", "fr-fr");
-  else encodedParams.set("hl", "hi-in");
+//   if (language === "ja") encodedParams.set("hl", "ja-jp");
+//   else if (language === "es") encodedParams.set("hl", "es-es");
+//   else if (language === "fr") encodedParams.set("hl", "fr-fr");
+//   else encodedParams.set("hl", "hi-in");
 
-  const { data }: { data: string } = await axios.post(
-    "https://voicerss-text-to-speech.p.rapidapi.com/",
-    encodedParams,
-    {
-      params: { key },
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-        "X-RapidAPI-Key": rapidKey,
-        "X-RapidAPI-Host": "voicerss-text-to-speech.p.rapidapi.com",
-      },
-    }
-  );
+//   const { data }: { data: string } = await axios.post(
+//     "https://voicerss-text-to-speech.p.rapidapi.com/",
+//     encodedParams,
+//     {
+//       params: { key },
+//       headers: {
+//         "content-type": "application/x-www-form-urlencoded",
+//         "X-RapidAPI-Key": rapidKey,
+//         "X-RapidAPI-Host": "voicerss-text-to-speech.p.rapidapi.com",
+//       },
+//     }
+//   );
 
-  return data;
-};
+//   return data;
+// };
